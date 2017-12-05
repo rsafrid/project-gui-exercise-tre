b69eadd0db39905475cba8b331cbc00040a94c7a
Susanne Fridh <fridh.susanne@gmail.com>
2017-12-05 09:24:55 +0100

    First commit for GUI exercise 3

---
+++ src/main/java/com/examle/gui/project_gui_exercise_tre/App.java
@@ -0,0 +1,13 @@
+package com.examle.gui.project_gui_exercise_tre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
